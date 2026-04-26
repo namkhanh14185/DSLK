5829e0d940ecf53ee367f3feee89368d4f61bb7f
namkhanh14185 <rbb6820@gmail.com>
2026-04-26 22:20:24 +0700

    f4

--- As2.cpp
+++ As2.cpp
@@ -3,13 +3,13 @@ using namespace std;
 struct Node 
 {
     int data;
-    Node *link;
+    Node*link;
 };
 
 struct List 
 {
-    Node *first;
-    Node *last;
+    Node*first;
+    Node*last;
 };
 
 void Init(List &l) 
@@ -29,15 +29,19 @@ Node*CreateNode(int x)
 void AddLast(List &l, Node *p) 
 {
     if (!p) return;
-    if (!l.first) {
+    if (!l.first) 
+    {
         l.first = l.last = p;
-    } else {
-        l.last->link = p;
-        l.last = p;
+    } 
+    else 
+    {
+        l.last->link=p;
+        l.last=p;
     }
-    l.last->link = l.first;
+    l.last->link=l.first;
 }
-void NhapDuLieu(int &n, int &m) {
+void NhapDuLieu(int &n, int &m) 
+{
     do {
         cout << "Nhap so nguoi choi (N > 0): ";
         cin >> n;
@@ -52,15 +56,22 @@ void NhapDuLieu(int &n, int &m) {
 }
 void Josephus(int n, int m) 
 {
-    List l; 
-    Init(l);
-    for (int i = 1; i <= n; i++) AddLast(l, CreateNode(i));
-    Node* p = l.first;
-    Node* prev = l.last;
-    cout<<"\n--- KET QUA LOAI ---" << endl;
-    cout<<"Buoc\tNguoi bi loai" << endl;
+    List l; Init(l);
+    for (int i = 1; i <= n; i++) 
+    {
+        AddLast(l, CreateNode(i));
+    }
+    Node*p=l.first;
+    Node*prev=l.last;
+    if (n <= 100) 
+    {
+        cout << "\nThu tu cac nguoi bi loai: ";
+    }
+    else 
+    {
+        cout << "\nDang tinh toan cho N = " << n << ". Vui long doi trong giay lat..." << endl;
+    }
 
-    int dem=0;
     while(p->link != p) 
     { 
         for(int i = 0; i < m; i++) 
@@ -68,23 +79,22 @@ void Josephus(int n, int m)
             prev = p;
             p = p->link;
         }
-        dem++;
-        cout<<dem<<"\tNguoi so "<<p->data<< endl;
-        prev->link=p->link;
+        if (n <= 100) cout << p->data << " ";
+        
+        prev->link = p->link;
         delete p;
-        p=prev->link;
+        p = prev->link;
     }
-    cout<<"--------------------"<< endl;
-    cout<<"\n=> NGUOI CHIEN THANG: "<<p->data << endl;
+    
+    cout << "\n=> NGUOI CHIEN THANG CUOI CUNG: " << p->data << endl;
     delete p;
 }
 
-int main() 
-{
+int main() {
     int n, m;
     NhapDuLieu(n, m);
     Josephus(n, m);
-    cout<<"\nNhan Enter de ket thuc...";
+    cout << "\nNhan Enter de ket thuc...";
     cin.ignore(); 
     cin.get();
     return 0;
