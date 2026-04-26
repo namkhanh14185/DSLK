882cae763a11cc58a4e9606068426ea169f6b95e
namkhanh14185 <rbb6820@gmail.com>
2026-04-26 15:16:27 +0700

    l3

--- DSLK.cpp
+++ DSLK.cpp
@@ -66,40 +66,45 @@ void AddLast(List &l, Node *p) {
         l.last=p;
     }
 }
-void PrintList(List l) 
+void DoiCho(SinhVien &a, SinhVien &b) 
 {
+    SinhVien tmp = a; 
+    a = b; 
+    b = tmp;
+}
+
+void Sapxep(List &l) 
+{
+    if (l.first == NULL) return;
+    for (Node *p1 = l.first; p1 != NULL; p1 = p1->link) {
+        for (Node *p2 = p1->link; p2 != NULL; p2 = p2->link) {
+            if (p1->data.maSV > p2->data.maSV) {
+                DoiCho(p1->data, p2->data);
+            }
+        }
+    }
+}
+
+void In(List l) {
     cout << "\nMa SV\t| Ho Ten\t\t| Lop\t| Ngay sinh" << endl;
-    cout << "--------------------------------------------------------" << endl;
     Node *p=l.first;
-    while(p!= NULL) 
-    {
-        cout << p->data.maSV << "\t| " 
-             << p->data.hoTen << "\t| "
-             << p->data.lop << "\t| "
-             << p->data.ngaySinh.ngay << "/" << p->data.ngaySinh.thang << "/" << p->data.ngaySinh.nam << endl;
+    while(p!= NULL) {
+        cout << p->data.maSV << "\t| " << p->data.hoTen << "\t| " << p->data.lop << "\t| " << p->data.ngaySinh.ngay << "/" << p->data.ngaySinh.thang << "/" << p->data.ngaySinh.nam << endl;
         p = p->link;
     }
 }
 
-int main() 
-{
-    List listSV;
-    Init(listSV);
-
-    int n;
-    cout << "Nhap so luong sinh vien: "; 
-    cin >> n;
-    
-    for(int i=0;i<n; i++)
-     {
-        cout << "\nNhap sinh vien thu " << i + 1 << ":" << endl;
-        SinhVien x;
-        NhapSV(x);
-        Node*p = CreateNode(x);
-        AddLast(listSV, p);
+int main() {
+    List l; Init(l);
+    int n; cout << "Nhap n: "; cin >> n;
+    for(int i=0; i<n; i++) {
+        cout << "SV " << i+1 << endl;
+        SinhVien x; NhapSV(x);
+        AddLast(l, CreateNode(x));
     }
-    cout << "\n--- DANH SACH VUA NHAP ---";
-    PrintList(listSV);
 
+    cout << "\nTruoc khi xep:"; In(l);
+    Sapxep(l);
+    cout << "\nSau khi xep:"; In(l);
     return 0;
 }
