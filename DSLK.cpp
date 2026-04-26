2c56c55feef71f3bbbae2757a931c449c7c17425
namkhanh14185 <rbb6820@gmail.com>
2026-04-26 16:31:11 +0700

    Them file As2.cpp cho bai toan Josephus

--- DSLK.cpp
+++ DSLK.cpp
@@ -7,13 +7,9 @@ struct Ngay {
 };
 
 struct SinhVien {
-    string maSV;
-    string hoTen;
+    string maSV, hoTen, diaChi, lop, khoa;
     int gioiTinh; 
     Ngay ngaySinh;
-    string diaChi;
-    string lop;
-    string khoa;
 };
 
 struct Node {
@@ -22,13 +18,10 @@ struct Node {
 };
 
 struct List {
-    Node *first;
-    Node *last;
+    Node *first, *last;
 };
 
-void Init(List &l) {
-    l.first = l.last = NULL;
-}
+void Init(List &l) { l.first = l.last = NULL; }
 
 Node* CreateNode(SinhVien x) {
     Node *p = new Node;
@@ -37,49 +30,34 @@ Node* CreateNode(SinhVien x) {
     p->link = NULL;
     return p;
 }
-
 void NhapSV(SinhVien &sv) {
-    cout<< "Nhap Ma SV: "; 
-    cin>>sv.maSV;
+    cout << "Nhap Ma SV: "; cin >> sv.maSV;
     cin.ignore(); 
-    cout<< "Nhap Ho Ten: "; 
-    getline(cin, sv.hoTen);
-    cout<< "Gioi tinh (0: Nam, 1: Nu): "; 
-    cin>>sv.gioiTinh;
-    cout<< "Nhap Ngay, Thang, Nam sinh: "; 
-    cin>>sv.ngaySinh.ngay >> sv.ngaySinh.thang >> sv.ngaySinh.nam;
-    cin.ignore();
-    cout << "Nhap Dia chi: "; 
-    getline(cin, sv.diaChi);
-    cout << "Nhap Lop: "; 
-    getline(cin, sv.lop);
-    cout << "Nhap Khoa: "; 
-    getline(cin, sv.khoa);
+    cout << "Nhap Ho Ten: "; getline(cin, sv.hoTen);
+    cout << "Gioi tinh (0: Nam, 1: Nu): "; cin >> sv.gioiTinh;
+    cout << "Nhap Ngay, Thang, Nam sinh (cach nhau khoang trang): "; 
+    cin >> sv.ngaySinh.ngay >> sv.ngaySinh.thang >> sv.ngaySinh.nam;
+    cin.ignore(); // Xoa dau xuong dong de getline phia sau ko bi loi
+    cout << "Nhap Dia chi: "; getline(cin, sv.diaChi);
+    cout << "Nhap Lop: "; getline(cin, sv.lop);
+    cout << "Nhap Khoa: "; getline(cin, sv.khoa);
 }
 
 void AddLast(List &l, Node *p) {
     if (p == NULL) return;
-    if (l.first == NULL) {
-        l.first=l.last=p;
-    } else {
-        l.last->link=p;
-        l.last=p;
-    }
+    if (l.first == NULL) l.first = l.last = p;
+    else { l.last->link = p; l.last = p; }
 }
 
 void DoiCho(SinhVien &a, SinhVien &b) {
-    SinhVien tmp = a; 
-    a = b; 
-    b = tmp;
+    SinhVien tmp = a; a = b; b = tmp;
 }
 
 void Sapxep(List &l) {
     if (l.first == NULL) return;
     for (Node *p1 = l.first; p1 != NULL; p1 = p1->link) {
         for (Node *p2 = p1->link; p2 != NULL; p2 = p2->link) {
-            if (p1->data.maSV > p2->data.maSV) {
-                DoiCho(p1->data, p2->data);
-            }
+            if (p1->data.maSV > p2->data.maSV) DoiCho(p1->data, p2->data);
         }
     }
 }
@@ -87,21 +65,18 @@ void Sapxep(List &l) {
 void ChenGiuThuTu(List &l, Node *p) {
     if (p == NULL) return;
     if (l.first == NULL || p->data.maSV < l.first->data.maSV) {
-        p->link = l.first;
-        l.first = p;
+        p->link = l.first; l.first = p;
         if (l.last == NULL) l.last = p;
         return;
     }
     Node *c = l.first;
-    while (c->link != NULL && c->link->data.maSV < p->data.maSV) {
-        c = c->link;
-    }
-    p->link = c->link;
-    c->link = p;
+    while (c->link != NULL && c->link->data.maSV < p->data.maSV) c = c->link;
+    p->link = c->link; c->link = p;
     if (p->link == NULL) l.last = p;
 }
 
-void TimTrungNgaySinh(List l) {
+void TimTrungNgaySinh(List l) 
+{
     bool co = false;
     cout << "\n--- Cac cap sinh vien cung ngay sinh ---" << endl;
     for (Node *p = l.first; p != NULL; p = p->link) {
@@ -114,42 +89,48 @@ void TimTrungNgaySinh(List l) {
             }
         }
     }
-    if (!co) cout << "Khong co sinh vien nao trung ngay sinh." << endl;
+    if (!co) 
+    cout<<"Khong co sinh vien nao trung ngay sinh." << endl;
 }
 
-void In(List l) {
-    cout<< "\nMa SV\t| Ho Ten\t\t| Lop\t| Ngay sinh" << endl;
-    Node*p=l.first;
-    while(p!= NULL) {
-        cout<< p->data.maSV << "\t| " << p->data.hoTen << "\t| " << p->data.lop << "\t| " << p->data.ngaySinh.ngay << "/" << p->data.ngaySinh.thang << "/" << p->data.ngaySinh.nam << endl;
+void In(List l) 
+{
+    cout << "\nMa SV\t| Ho Ten\t\t| Lop\t| Ngay sinh" << endl;
+    Node *p = l.first;
+    while(p != NULL) {
+        cout << p->data.maSV << "\t| " << p->data.hoTen << "\t| " << p->data.lop << "\t| " 
+             << p->data.ngaySinh.ngay << "/" << p->data.ngaySinh.thang << "/" << p->data.ngaySinh.nam << endl;
         p = p->link;
     }
 }
 
-int main() {
-    List l; Init(l);
+int main() 
+{
+    List l; 
+    Init(l);
     int n; 
-    cout<<"Nhap n: ";
+    cout<<"Nhap n: "; 
     cin>>n;
     for(int i=0; i<n; i++) 
     {
-        cout<<"SV"<<i+1<<endl;
+        cout<<"\nNhap SV "<<i+1<<":"<< endl;
         SinhVien x; 
         NhapSV(x);
         AddLast(l, CreateNode(x));
     }
 
     Sapxep(l);
-    cout<<"\nSau khi xep:"; In(l);
-    cout<<"\nNhap 1 SV moi de chen:";
+    cout<<"\nSau khi xep:"; 
+    In(l);
+    cout<<"\n--- NHAP 1 SV MOI DE CHEN ---"<< endl;
     SinhVien svMoi; 
     NhapSV(svMoi);
-    ChenGiuThuTu(l, 
-        CreateNode(svMoi));
-    
-    cout<<"\nSau khi chen:"; In(l);
+    ChenGiuThuTu(l, CreateNode(svMoi));
+    cout<< "\nSau khi chen:"; 
+    In(l);
     TimTrungNgaySinh(l);
     cout<<"\nNhan Enter de thoat...";
-    system("pause"); 
+    cin.ignore(); 
+    cin.get(); 
     return 0;
 }
