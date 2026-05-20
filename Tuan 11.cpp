60babe5172f6becf6a399d1d76a6d9f9faf76f51
namkhanh14185 <rbb6820@gmail.com>
2026-05-20 10:34:49 +0700

    fx

--- Tuan 11.cpp
+++ Tuan 11.cpp
@@ -19,14 +19,34 @@ PNode taoNut(string x) {
 
 int main() {
     PNode root = taoNut("-");
+    
     PNode chia = taoNut("/");
-    PNode mu = taoNut("^");
+    PNode cong = taoNut("+");
+    PNode nhan1 = taoNut("*");
+    PNode a = taoNut("a");
+    PNode b = taoNut("b");
+    PNode c = taoNut("c");
+    PNode d = taoNut("d");
     
     root->left = chia;
-    root->right = mu;
+    chia->left = cong; chia->right = d;
+    cong->left = nhan1; cong->right = c;
+    nhan1->left = a; nhan1->right = b;
     
-    cout << "Nut goc: " << root->info << endl;
-    cout << "Con trai: " << root->left->info << " , Con phai: " << root->right->info << endl;
+    PNode mu = taoNut("^");
+    PNode tru = taoNut("-");
+    PNode nhan2 = taoNut("*");
+    PNode e = taoNut("e");
+    PNode f = taoNut("f");
+    PNode h = taoNut("h");
+    PNode g = taoNut("g");
+    
+    root->right = mu;
+    mu->left = tru; mu->right = g;
+    tru->left = nhan2; tru->right = h;
+    nhan2->left = e; nhan2->right = f;
+    
+    cout << "Cay bieu thuc da duoc dung xong." << endl;
     
     return 0;
 }
